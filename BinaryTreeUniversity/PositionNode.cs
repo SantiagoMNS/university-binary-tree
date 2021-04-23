54f0a1e36fe182bb7317b77cb40d67dd8ba00eb3
SantiagoMNS <snorena79777@umanizales.edu.co>
2021-04-22 23:25:16 -0500

    first commit

---
+++ BinaryTreeUniversity/PositionNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeUniversity
+{
+    class PositionNode
+    {
+        public Position Position;
+        public PositionNode left;
+        public PositionNode Righ;
+
+    }
+}
